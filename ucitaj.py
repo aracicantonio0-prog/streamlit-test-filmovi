14a46695c02dc416d5ba9bb85f2127d3e2569e46
aracicantonio0-prog <aracicantonio0@gmail.com>
2025-11-20 20:04:24 +0100

    Add files via upload

---
+++ ucitaj.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import gspread as gs
+
+from google.oauth2.service_account import Credentials
+
+
+def ucitaj_podatke(sheet_url, sheet_name):
+    scope = [
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive"
+    ]
+    
+    creds_info = st.secrets["gcp_service_account"]
+    creds = Credentials.from_service_account_info(creds_info, scopes=scope)
+    client = gs.authorize(creds)
+    sheet = client.open_by_url(sheet_url)
+    worksheet = sheet.worksheet(sheet_name)
+    data = worksheet.get_all_records()
+    return pd.DataFrame(data), worksheet
